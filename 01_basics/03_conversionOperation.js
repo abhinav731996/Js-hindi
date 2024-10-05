a777a70586e271d9c7e50295d4c674dba810a60b
Abhinav <abhinav731996@gmail.com>
2024-10-05 09:30:35 +0000

    conversion od datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueinnumber = Number(score)
+console.log(typeof valueinnumber);
+console.log(valueinnumber);
+
+/*
+1. "33" = 33
+2. "33abc" = NaN
+3. true/fals = 1/0
+*/
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean (isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+/*
+1. 1=true, 0=false
+2. ""=false
+3. "Abhinav"=true
+*/
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
