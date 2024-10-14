60a51082965eee99b937f9c93e9fdb7eca7fb999
Abhinav <abhinav731996@gmail.com>
2024-10-14 05:04:57 +0000

    functions

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -28,3 +28,33 @@ function loginUserMessage(username) {
 // console.log(loginUserMessage("Abhinav"))
 console.log(loginUserMessage());
 
+//+++++++++++ Calculate +++++++++++++++
+function calculate(...num1) {
+    return num1
+}
+console.log(calculate(200, 400, 500, 2000));
+
+function calculateNum(val1, val2, ...num2) {
+    return num2
+}
+console.log(calculateNum(20, 40, 50, 200));
+
+const user = {
+    userNa : "Abhinav",
+    price : 07
+}
+function handleObject(anyobject) {
+    console.log(`username is ${anyobject.userNa} and price is ${anyobject.price}`);
+    
+}
+handleObject(user)
+handleObject({
+    userNa : "Sam",
+    price : 10
+})
+
+const myNewArray = [100, 200, 400, 600]
+function returnsecondvalue(getarray) {
+    return getarray[2]
+}
+console.log(returnsecondvalue(myNewArray));
